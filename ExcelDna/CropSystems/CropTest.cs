2dbf3c190ac4ea30eec032b95ab19cb8a8a8039f
James Bristow <james.bristow@plantandfood.co.nz>
2023-03-05 18:07:52 +1300

    Adding crop system test

---
+++ ExcelDna/CropSystems/CropTest.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using ExcelDna.Integration;
+using Microsoft.Data.Analysis;
+
+public static class CropTest
+{
+
+    /// <summary>
+    /// Print crop data into an Excel spreadsheet.
+    /// </summary>
+    /// <returns>The crop dataframe as a string</returns>
+    [ExcelFunction(Description = "Load Dataframe Data into Excel")]
+    public static string PrintCropData()
+    {
+        DataFrame cropData = LoadCropData();
+        string cropDataString = cropData.ToString();
+        return cropDataString;
+    }
+
+
+    /// <summary>
+    /// Load crop data as an embedded resource into a dataframe
+    /// </summary>
+    /// <returns>A dataframe containing the crop data</returns>
+    public static DataFrame LoadCropData()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        string fileName = "crop_data.csv";
+        string resourceName = assembly.GetManifestResourceNames()
+            .Single(str => str.EndsWith(fileName));
+
+        Stream csv = assembly.GetManifestResourceStream(resourceName);
+        DataFrame cropData = DataFrame.LoadCsv(csv);
+        return cropData;
+    }
+
+}
